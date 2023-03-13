e5f64b8897cd0c26398ec0d9919ff1e64424912f
Jamie Vangeysel <32323785+JamieVangeysel@users.noreply.github.com>
2023-03-13 10:14:04 +0100

    remove unused code

--- src/app/app.server.module.ts
+++ src/app/app.server.module.ts
@@ -1,17 +1,11 @@
 import { NgModule } from '@angular/core'
 import { ServerModule } from '@angular/platform-server'
-import { Location } from '@angular/common'
 
 import { AppModule } from './app.module'
 import { AppComponent } from './app.component'
 import { translateServerLoaderFactory } from './@shared/loaders/translate-server.loader'
-import { TranslateLoader, TranslateModule, TranslateService } from '@ngx-translate/core'
-import { LocalizeRouterSettings, ManualParserLoader } from '@gilsdav/ngx-translate-router'
-import { environment } from 'src/environments/environment'
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core'
 
-export function localizeLoaderFactory(translate: TranslateService, location: Location, settings: LocalizeRouterSettings) {
-  return new ManualParserLoader(translate, location, settings, environment.supportedLanguages, 'ROUTES')
-}
 
 @NgModule({
   imports: [
